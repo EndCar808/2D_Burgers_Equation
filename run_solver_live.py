0167e93c2d5156bede227f23e4e5318cde7b2663
EndCar808 <endacar808@gmail.com>
2022-04-07 09:39:13 +0100

    Updated run_solver_live.py script

--- run_solver_live.py
+++ run_solver_live.py
@@ -64,19 +64,19 @@ if __name__ == '__main__':
 	Nx             = 128
 	Ny             = 128
 	t0             = 0.0
-	T              = 1.0
+	h              = 1e-4
+	T              = 50 * h
 	c              = 0.7
-	h              = 1e-3
-	v              = 1e2
+	v              = 1e-5
 	hypervisc      = int(1)
 	ekmn_alpha     = 0.0
 	ekmn_hypo_diff = -int(2)
-	u0             = "TG_VEL_POT"
-	s_tag          = "TG-Live"
+	u0             = "HOPF_COLE"
+	s_tag          = "Live"
 	forcing 	   = 0
 	force_k        = 0
-	ndata          = 50
-	save_every     = int((T - t0) / (h * ndata))
+	ndata          = 10
+	save_every     = 1 ## int((T - t0) / (h * ndata))
 
 	## Clean output directory
 	run(["rm -r {}".format(output_dir + "*")], shell = True)
@@ -85,7 +85,7 @@ if __name__ == '__main__':
 	##      RUN SOLVER     ##
 	#########################
 	## Generate command list 
-	cmd_list = ["mpirun -n {} {} -o {} -n {} -n {} -s {:3.1f} -e {:3.1f} -c {:1.6f} -h {:1.6f} -v {:1.10f} -v {} -a {:1.6f} -a {} -i {} -t {} -f {} -f {} -p {}".format(
+	cmd_list = ["mpirun -n {} {} -o {} -n {} -n {} -s {:3.1f} -e {:3.6f} -c {:1.6f} -h {:1.6f} -v {:1.10f} -v {} -a {:1.6f} -a {} -i {} -t {} -f {} -f {} -p {}".format(
 				solver_procs, 
 				executable, 
 				output_dir, 
@@ -106,9 +106,10 @@ if __name__ == '__main__':
 	##      DISPLAY DATA     ##
 	###########################
 	## Read in data for plotting
-	data_file = output_dir + os.listdir(output_dir)[0] + "/Main_HDF_Data.h5"
-	print("\nData File:" + tc.C + "{}".format(data_file) + tc.Rst)
-	with h5py.File(data_file, 'r') as data:
+	data_file = output_dir + os.listdir(output_dir)[0]
+	with h5py.File(data_file + "/Main_HDF_Data.h5", 'r') as data:
+		print("\nData File: " + tc.C + "{}".format(data_file + "/Main_HDF_Data.h5") + tc.Rst)
+		
 		## Get the number of snapshots
 		ndata = len([g for g in data.keys() if 'Iter' in g])
 
@@ -124,22 +125,68 @@ if __name__ == '__main__':
 			if "Iter" in group:
 				if 'psi' in list(data[group].keys()):
 					psi[nn, :, :] = data[group]["psi"][:, :]
-			nn += 1
+				nn += 1
+		if 'Time' in list(data.keys()):
+			time = data['Time'][:]
+		if 'TotalEnergy' in list(data.keys()):
+			tot_enrg = data['TotalEnergy'][:]
+		if 'TotalDivergenceSquared' in list(data.keys()):
+			tot_div_sqr = data['TotalDivergenceSquared'][:]
+		if 'Totaluv' in list(data.keys()):
+			tot_uv = data['Totaluv'][:]
+		if 'TotaluSqrvSqr' in list(data.keys()):
+			tot_u_sqr_v_sqr = data['TotaluSqrvSqr'][:]
+
+	with h5py.File(data_file + "/Spectra_HDF_Data.h5", 'r') as data:
+		print("\nSpectra File: " + tc.C + "{}".format(data_file + "/Spectra_HDF_Data.h5") + tc.Rst)
+		
+		## Get the number of snapshots
+		ndata = len([g for g in data.keys() if 'Iter' in g])
+
+		print("\nNumber of snapshots: " + tc.C + "{}\n".format(ndata) + tc.Rst)
+
+		## Get the data
+		enrg_spec = np.zeros((ndata, int(np.sqrt((Nx/2)**2 + (Ny/2)**2) + 1)))
+
+		nn = 0
+
+		# Read in the data
+		for group in data.keys():
+			if "Iter" in group:
+				if 'EnergySpectrum' in list(data[group].keys()):
+					enrg_spec[nn, :] = data[group]["EnergySpectrum"][:]
+				nn += 1
 
 	## Plot data
-	fig = plt.figure()
-	ax  = fig.add_subplot(111)
-	im  = ax.imshow(psi[0, :, :], norm=None, cmap="RdBu")
-	cax = make_axes_locatable(ax).append_axes("right", size="5%", pad="2%")
-	cb  = fig.colorbar(im, cax=cax)
-	ax.set_xticks([])
-	ax.set_yticks([])
+	fig  = plt.figure(figsize = (21, 9))
+	ax1  = fig.add_subplot(131)
+	im   = ax1.imshow(psi[0, :, :] / np.sqrt(np.mean(psi[0, :, :]**2)), norm=None, cmap="RdBu")
+	cax1 = make_axes_locatable(ax1).append_axes("right", size="5%", pad="2%")
+	cb   = fig.colorbar(im, cax=cax1)
+	ax1.set_xticks([])
+	ax1.set_yticks([])
+	ax2  = fig.add_subplot(132)
+	ax2.grid()
+	ax2.plot(np.arange(1, int(Nx/3 + 1)), enrg_spec[0, 1:int(Nx/3 + 1)])
+	ax3  = fig.add_subplot(133)
+	ax3.plot(time[:0], tot_enrg[:0])
+	ax3.set_xlim(time[0], time[-1])
+	ax3.grid()
 	for i in range(1, ndata):
-		im.set_data(psi, norm=None, cmap="RdBu")
-		plt.title(r"Iter: {}".format(i))
+		im.set_data(psi[i, :, :] / np.sqrt(np.mean(psi[i, :, :]**2)))
+		ax2.plot(np.arange(1, int(Nx/3 + 1)), enrg_spec[i, 1:int(Nx/3 + 1)], 'b')
+		ax2.set_xscale('log')
+		ax2.set_yscale('log')
+		ax2.grid()
+		ax3.plot(time[:i], tot_enrg[:i], 'b')
+		ax3.set_xlim(time[0], time[-1])
+		ax3.grid()
+
+		plt.suptitle(r"Iter: {}".format(i))
 		fig.canvas.draw()
 		fig.canvas.flush_events()
-		plt.pause(1e-9)
+		plt.pause(1e-5)
+		wait = input("Press " + tc.C + "ENTER" + tc.Rst + " to continue.")
 
 	#######################
 	##      CLEAN UP     ##
